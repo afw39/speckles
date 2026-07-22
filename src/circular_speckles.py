3b5ea5fddc52c10bde49605ff303f6d536d05dc2
afw39 <aoife.willis@ukaea.uk>
2026-07-22 16:07:12 +0100

    adding an examples folder?

--- src/circular_speckles.py
+++ src/circular_speckles.py
@@ -111,8 +111,6 @@ def fftanalysis():
     fft_speckle_size = 1/(freq[peaks[2]])
     return fft_speckle_size
 
-
-
 imagewidth, imageheight, speckle_radius, blackwhite = userinputs()
 imagesize = imagewidth * imageheight
 speckle_size = np.pi * (speckle_radius**2)

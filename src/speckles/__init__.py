c9b4b107a7f08882696f72f3297d583f88ebeb10
afw39 <aoife.willis@ukaea.uk>
2026-07-23 15:57:42 +0100

    have put my pattern generation and fft in two different files successfully

--- src/speckles/__init__.py
+++ src/speckles/__init__.py
@@ -1,3 +1,5 @@
 #importing stuff
-from .speckle_generator import(generate_pattern, 
-                               fftanalysis)
+#from speckles.speckle_generator import generate_pattern
+#from speckles.speckle_generator import fftanalysis
+from .pattern import generate_pattern
+from .fft import fftanalysis
---
+++ src/speckles/pattern.py
@@ -0,0 +1,53 @@
+#imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+#pattern generation
+def generate_pattern(imagewidth, imageheight, speckle_radius, blackwhite, save):
+    print(f"generating {imagewidth} x {imageheight}")
+    imagesize = imagewidth * imageheight
+    speckle_size = np.pi * (speckle_radius**2)
+    number_of_speckles = imagesize * blackwhite / speckle_size
+    speckle_spacing = np.sqrt(imagesize / number_of_speckles)
+
+    x_coords = np.arange(0, imagewidth, speckle_spacing)
+    y_coords = np.arange(0, imageheight, speckle_spacing)
+    X, Y = np.meshgrid(x_coords, y_coords) #making my grid
+    #plt.scatter(X,Y) remove the hash if want to see the original grid - ask a user input for this
+
+    #generating random displacements
+    x_disp = np.random.randint(
+        (-1 * speckle_spacing // 2) + 1,
+        (speckle_spacing //2) + 1,
+        size = X.shape)
+
+    y_disp = np.random.randint(
+        (-1 * speckle_spacing // 2) + 1,
+        (speckle_spacing // 2) + 1,
+        size = Y.shape)
+    
+    #adding my random displacements to each grid point
+    X_new = X + x_disp 
+    Y_new = Y + y_disp
+    image = np.full((imageheight, imagewidth), 1.0)
+    # splitting each pixel into 16 - 4x4 subpixels
+    samples = 4
+    offsets = (np.arange(samples) + 0.5) / samples - 0.5
+
+    yy, xx = np.meshgrid(np.arange(imageheight),np.arange(imagewidth),indexing = 'ij')
+
+    for x, y in zip(X_new.ravel(), Y_new.ravel()):
+        coverage = np.zeros_like(image, dtype = float)
+
+        for dx in offsets:
+            for dy in offsets:
+                dist2 = ((xx+dx)-x)**2 + ((yy+dy)-y)**2
+                coverage += dist2 <= speckle_radius**2
+        coverage /= samples**2
+        #need it to be greyscale proportional to how much pixel is being covered
+        image = np.minimum(image, 1 - coverage)
+    plt.imshow(image, cmap = 'gray', vmin = 0, vmax = 1)
+    if save:
+        plt.savefig('new_speckle_pattern.tiff')
+    return image
+

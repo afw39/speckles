689b8cdfd757212ff3057c2596a61a9320924e51
afw39 <aoife.willis@ukaea.uk>
2026-07-23 17:31:29 +0100

    commiting the changes in second example script - added the description

--- examples/example_script_two.py
+++ examples/example_script_two.py
@@ -5,7 +5,13 @@ from speckles import alternate_pattern_generation
 
 def main():
     '''
-    description
+    Alternate speckle generator: generates speckles as squares rather than circles. Does not generate points from a random displacement from a grid but just fills in random pixels on the image. 
+    - imageheight (int) = dimension of the image in pixels
+    - imagewidth (int) = dimension of the image in pixels
+    - speckleheight (int) = height of speckle in pixels
+    - specklewidth (int) = width of speckle in pixels
+    - bwbalance (float) = ratio of black pixels to white pixels (~density of speckles) between 0.0 and 1.0 (0 is black, 1.0 is white)
+    - save (bool) = determines if speckle patten is saved or not (saved in lossless format of a tiff) 
     '''
 
     #inputs

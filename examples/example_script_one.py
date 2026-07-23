c39306b8acaf97dcd7bbd2b533fde17a047a0cdf
afw39 <aoife.willis@ukaea.uk>
2026-07-23 12:46:46 +0100

    added my first example script?

--- examples/example_script_one.py
+++ examples/example_script_one.py
@@ -1,17 +1,8 @@
 #imports at the top
-from speckles import generate_pattern
-from speckles import fftanalysis
-
+from speckles import generate_pattern, fftanalysis
 
 #enter dimensions for image (pixels), the black-white balance of the speckle pattern and the speckle radius
 
-imageheight = 50
-imagewidth = 50
-blackwhite = 0.5
-speckle_radius = 5
-save = True 
-visualfft = True
-
 #generating the speckle pattern/fft (if necessary). put 'save = True' above if you want the file(s) saved. 
-generate_pattern(imagewidth, imageheight, speckle_radius, blackwhite, save)
-fftanalysis(visualfft)
+image = generate_pattern(imagewidth = 500, imageheight = 500, speckle_radius = 5, blackwhite = 0.5, save = True)
+fftanalysis(image,visualfft = True)

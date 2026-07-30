e9fc40755a1908b9081c4bafe7f7d421dd8f83e7
afw39 <aoife.willis@ukaea.uk>
2026-07-30 15:57:07 +0100

    fixed the wwording regarding dots on speckle pattern in docstrings

--- src/examples/example_script_two.py
+++ src/examples/example_script_two.py
@@ -3,12 +3,12 @@ from speckles import alternate_pattern_generation
 
 def main():
     '''
-    Alternate speckle generator: generates speckles as squares rather than circles. Does not generate points from a random displacement from a grid but just fills in random pixels on the image. 
+    Alternate speckle generator: generates speckle pattern using rectangles. Does not generate points from a random displacement from a grid but just fills in random pixels on the image. 
     - imageheight (int) = dimension of the image in pixels
     - imagewidth (int) = dimension of the image in pixels
-    - speckleheight (int) = height of speckle in pixels
-    - specklewidth (int) = width of speckle in pixels
-    - bwbalance (float) = ratio of black pixels to white pixels (~density of speckles) between 0.0 and 1.0 (1.0 is most speckles, 0 is no speckles)
+    - speckleheight (int) = height of rectangle on speckle pattern in pixels
+    - specklewidth (int) = width of rectangle on speckle pattern in pixels
+    - bwbalance (float) = ratio of black pixels to white pixels (~density of black pixels) between 0.0 and 1.0 (1.0 is most black, 0 is all white pixels)
     - save (bool) = determines if speckle patten is saved or not (saved in lossless format of a tiff) 
     '''
 

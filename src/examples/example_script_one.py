ea85a92ed0d9fed4c50b9d1017bfcc7163fef076
afw39 <aoife.willis@ukaea.uk>
2026-07-31 17:20:41 +0100

    changing all speckles to dots

--- src/examples/example_script_one.py
+++ src/examples/example_script_one.py
@@ -6,7 +6,7 @@ def main() -> None:
     Speckle pattern example: generates a speckle pattern based on variables:
     imagewidth (int) = dimension for image width in pixels
     imageheight (int) = dimension for image height in pixels
-    speckle_radius (int) = radius of the dots in the speckle pattern in pixels
+    dot_radius (int) = radius of the dots in the speckle pattern in pixels
     blackwhite (float) = ratio of black to white pixels generated - determines speckle number/spacing (1.0 for 100% dots, 0.0 for no dots). For e.g 0.5 - won't be exactly 50% black and 50% white pixels as the speckles will overlap (so put the number a bit closer to one/bigger than desired) 
     grid (bool) = determines if original grid is printed over speckle pattern (to see displacements from original points - not reccommended)
     save (bool) = determines if speckle pattern is saved or not
